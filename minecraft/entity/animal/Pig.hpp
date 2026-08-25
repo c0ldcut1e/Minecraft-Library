7736188fdaa26b40ee97aecdb4c2e63757a17b0d
Yuki <deinemuddayuki123@gmail.com>
2026-08-25 22:40:32 +0200

    Complete recovered pig fields and API

--- minecraft/entity/animal/Pig.hpp
+++ minecraft/entity/animal/Pig.hpp
@@ -23,6 +23,26 @@ namespace mc
             return MLINK_FUNC(uint64_t, 0x027CE7D0)();
         }
 
+        static Pig *Create(Level *level)
+        {
+            return MLINK_FUNC(Pig *, 0x023E6670, Level *)(level);
+        }
+
+        void registerGoals()
+        {
+            MLINK_FUNC(void, 0x027599C4, Pig *)(this);
+        }
+
+        void registerAttributes()
+        {
+            MLINK_FUNC(void, 0x02759E58, Pig *)(this);
+        }
+
+        bool canBeControlledByRider()
+        {
+            return MLINK_FUNC(bool, 0x02759EF8, Pig *)(this);
+        }
+
         void defineSynchedData()
         {
             MLINK_FUNC(void, 0x0275A7F4, Pig *)(this);
@@ -38,15 +58,39 @@ namespace mc
             MLINK_FUNC(void, 0x0275AB24, Pig *, bool)(this, value);
         }
 
-        bool field_0x780;
+        bool causeFallDamage(float fallDistance, float damageMultiplier)
+        {
+            return MLINK_FUNC(bool, 0x0275D29C, Pig *, float, float)(this, fallDistance, damageMultiplier);
+        }
+
+        void travel(float strafe, float vertical, float forward)
+        {
+            MLINK_FUNC(void, 0x0275D4BC, Pig *, float, float, float)(this, strafe, vertical, forward);
+        }
+
+        bool boost()
+        {
+            return MLINK_FUNC(bool, 0x0275DB98, Pig *)(this);
+        }
+
+        bool testboost()
+        {
+            return MLINK_FUNC(bool, 0x0275DCB0, Pig *)(this);
+        }
+
+        bool removeWhenFarAway()
+        {
+            return MLINK_FUNC(bool, 0x0275E16C, Pig *)(this);
+        }
+
+        bool boosting;
         uint8_t field_0x781;
         uint8_t field_0x782;
         uint8_t field_0x783;
-        uint32_t field_0x784;
-        uint32_t field_0x788;
+        int boostTime;
+        int boostTimeTotal;
         uint32_t field_0x78C;
-        uint32_t field_0x790;
-        uint32_t field_0x794;
+        uint64_t boostStartTime;
     };
     MC_CHECK_SIZE(Pig, 0x798);
 } // namespace mc
