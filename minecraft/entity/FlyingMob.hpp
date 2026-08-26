80f5e1fe7334b0624acd5e95c69b0c73430d7271
Yuki <deinemuddayuki123@gmail.com>
2026-08-26 02:19:55 +0200

    Add recovered flying mob entities

---
+++ minecraft/entity/FlyingMob.hpp
@@ -0,0 +1,32 @@
+#pragma once
+
+#include "mlink/MLink.hpp"
+
+#include "MinecraftLib.hpp"
+#include "entity/Mob.hpp"
+#include "world/Level.hpp"
+
+namespace mc
+{
+    class FlyingMob : public Mob
+    {
+    public:
+        FlyingMob() = default;
+
+        FlyingMob(Level *level)
+        {
+            MLINK_FUNC(void, 0x0241BAC8, FlyingMob *, Level *)(this, level);
+        }
+
+        void travel(float x, float y, float z)
+        {
+            MLINK_FUNC(void, 0x0241BB58, FlyingMob *, float, float, float)(this, x, y, z);
+        }
+
+        bool onLadder()
+        {
+            return MLINK_FUNC(bool, 0x0241C1A4, FlyingMob *)(this);
+        }
+    };
+    MC_CHECK_SIZE(FlyingMob, 0x708);
+} // namespace mc
---
+++ minecraft/entity/monster/Ghast.hpp
@@ -0,0 +1,55 @@
+#pragma once
+
+#include <cstdint>
+
+#include "mlink/MLink.hpp"
+
+#include "MinecraftLib.hpp"
+#include "entity/FlyingMob.hpp"
+#include "world/Level.hpp"
+
+namespace mc
+{
+    class Ghast : public FlyingMob
+    {
+    public:
+        Ghast(Level *level)
+        {
+            MLINK_FUNC(void, 0x02457BA0, Ghast *, Level *)(this, level);
+        }
+
+        static Ghast *Create(Level *level)
+        {
+            return MLINK_FUNC(Ghast *, 0x023E654C, Level *)(level);
+        }
+
+        static uint64_t GetType()
+        {
+            return MLINK_FUNC(uint64_t, 0x0246FC90)();
+        }
+
+        void registerGoals()
+        {
+            MLINK_FUNC(void, 0x02457E60, Ghast *)(this);
+        }
+
+        void defineSynchedData()
+        {
+            MLINK_FUNC(void, 0x024586C4, Ghast *)(this);
+        }
+
+        void registerAttributes()
+        {
+            MLINK_FUNC(void, 0x02458754, Ghast *)(this);
+        }
+
+        void tick()
+        {
+            MLINK_FUNC(void, 0x02458B28, Ghast *)(this);
+        }
+
+        uint32_t field_0x708;
+        uint32_t field_0x70C;
+    };
+    MC_CHECK_SIZE(Ghast, 0x710);
+} // namespace mc
