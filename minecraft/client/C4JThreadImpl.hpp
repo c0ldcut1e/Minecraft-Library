37ef366a29d83b34186fe165a8e07b47ab6c44b5
Yuki <deinemuddayuki123@gmail.com>
2026-08-26 12:42:58 +0200

    Expand recovered thread API

--- minecraft/client/C4JThreadImpl.hpp
+++ minecraft/client/C4JThreadImpl.hpp
@@ -16,9 +16,69 @@ namespace mc
             MLINK_FUNC(void, 0x034ACAC0, C4JThreadImpl *, int (*)(void *), void *, const char *, int)(this, function, data, name, stackSize);
         }
 
+        C4JThreadImpl(const char *name)
+        {
+            MLINK_FUNC(void, 0x034ACC90, C4JThreadImpl *, const char *)(this, name);
+        }
+
+        ~C4JThreadImpl()
+        {
+            MLINK_FUNC(void, 0x034ACE48, C4JThreadImpl *)(this);
+        }
+
+        static C4JThreadImpl *getCurrentThread()
+        {
+            return MLINK_FUNC(C4JThreadImpl *, 0x034ABAA8)();
+        }
+
+        static bool isMainThread()
+        {
+            return MLINK_FUNC(bool, 0x034ABBB0)();
+        }
+
+        static const char *getCurrentThreadName()
+        {
+            return MLINK_FUNC(const char *, 0x034ABC00)();
+        }
+
         bool isRunning()
         {
             return MLINK_FUNC(bool, 0x034ACFCC, C4JThreadImpl *)(this);
+        }
+
+        bool hasStarted()
+        {
+            return MLINK_FUNC(bool, 0x034ACFD4, C4JThreadImpl *)(this);
+        }
+
+        void SetProcessor(int processor)
+        {
+            MLINK_FUNC(void, 0x034ACFDC, C4JThreadImpl *, int)(this, processor);
+        }
+
+        int GetProcessor()
+        {
+            return MLINK_FUNC(int, 0x034AD01C, C4JThreadImpl *)(this);
+        }
+
+        void SetPriority(int priority)
+        {
+            MLINK_FUNC(void, 0x034AD024, C4JThreadImpl *, int)(this, priority);
+        }
+
+        int WaitForCompletion(int timeoutMs)
+        {
+            return MLINK_FUNC(int, 0x034AD07C, C4JThreadImpl *, int)(this, timeoutMs);
+        }
+
+        int GetExitCode()
+        {
+            return MLINK_FUNC(int, 0x034AD0A0, C4JThreadImpl *)(this);
+        }
+
+        const char *getName()
+        {
+            return MLINK_FUNC(const char *, 0x034AD0A8, C4JThreadImpl *)(this);
         }
 
         void Run()
