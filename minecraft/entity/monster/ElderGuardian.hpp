324a2f4b13ce8b3d13a866151921c8bd466216b2
Yuki <deinemuddayuki123@gmail.com>
2026-08-26 02:11:42 +0200

    Add recovered guardian entities

---
+++ minecraft/entity/monster/ElderGuardian.hpp
@@ -0,0 +1,42 @@
+#pragma once
+
+#include <cstdint>
+
+#include "mlink/MLink.hpp"
+
+#include "MinecraftLib.hpp"
+#include "entity/monster/Guardian.hpp"
+#include "world/Level.hpp"
+
+namespace mc
+{
+    class ElderGuardian : public Guardian
+    {
+    public:
+        ElderGuardian(Level *level)
+        {
+            MLINK_FUNC(void, 0x022DE384, ElderGuardian *, Level *)(this, level);
+        }
+
+        static ElderGuardian *Create(Level *level)
+        {
+            return MLINK_FUNC(ElderGuardian *, 0x023E60D0, Level *)(level);
+        }
+
+        static uint64_t GetType()
+        {
+            return MLINK_FUNC(uint64_t, 0x02306A0C)();
+        }
+
+        void registerAttributes()
+        {
+            MLINK_FUNC(void, 0x022DE488, ElderGuardian *)(this);
+        }
+
+        void setGhost()
+        {
+            MLINK_FUNC(void, 0x022DE58C, ElderGuardian *)(this);
+        }
+    };
+    MC_CHECK_SIZE(ElderGuardian, 0x758);
+} // namespace mc
---
+++ minecraft/entity/monster/Guardian.hpp
@@ -0,0 +1,70 @@
+#pragma once
+
+#include <cstdint>
+
+#include "mlink/MLink.hpp"
+
+#include "MinecraftLib.hpp"
+#include "entity/Monster.hpp"
+#include "world/Level.hpp"
+
+namespace mc
+{
+    class Guardian : public Monster
+    {
+    public:
+        Guardian() = default;
+
+        Guardian(Level *level, bool elder)
+        {
+            MLINK_FUNC(void, 0x0245AF70, Guardian *, Level *, bool)(this, level, elder);
+        }
+
+        static Guardian *Create(Level *level)
+        {
+            return MLINK_FUNC(Guardian *, 0x023E6654, Level *)(level);
+        }
+
+        static uint64_t GetType()
+        {
+            return MLINK_FUNC(uint64_t, 0x0246FEF8)();
+        }
+
+        void registerGoals()
+        {
+            MLINK_FUNC(void, 0x0245B334, Guardian *)(this);
+        }
+
+        void registerAttributes()
+        {
+            MLINK_FUNC(void, 0x0245B5B0, Guardian *)(this);
+        }
+
+        void defineSynchedData()
+        {
+            MLINK_FUNC(void, 0x0245B6F8, Guardian *)(this);
+        }
+
+        void setActiveAttackTarget(int entityId)
+        {
+            MLINK_FUNC(void, 0x0245B790, Guardian *, int)(this, entityId);
+        }
+
+        void aiStep()
+        {
+            MLINK_FUNC(void, 0x0245C218, Guardian *)(this);
+        }
+
+        uint32_t field_0x730;
+        uint32_t field_0x734;
+        uint32_t field_0x738;
+        uint32_t field_0x73C;
+        uint32_t field_0x740;
+        uint32_t field_0x744;
+        uint32_t field_0x748;
+        uint32_t field_0x74C;
+        uint32_t field_0x750;
+        uint32_t field_0x754;
+    };
+    MC_CHECK_SIZE(Guardian, 0x758);
+} // namespace mc
