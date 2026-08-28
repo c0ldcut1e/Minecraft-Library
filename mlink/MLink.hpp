b022776398dcc176e603d761165c91a7b73743fc
21toffelz <toffelzpro@outlook.fr>
2026-08-28 05:28:53 +0200

    added toInt function and removed PLATFORM_CEMU

--- mlink/MLink.hpp
+++ mlink/MLink.hpp
@@ -2,57 +2,52 @@
 
 #include <cstdint>
 
-// Uncomment this if this library is used on Cemu
-// #define PLATFORM_CEMU
+#define MLINK_FUNC(ret, address, ...)                                          \
+  reinterpret_cast<ret (*)(__VA_ARGS__)>(address)
 
-#define MLINK_FUNC(ret, address, ...) reinterpret_cast<ret (*)(__VA_ARGS__)>(address)
+namespace MLink {
+inline int toInt(float input) {
+  bool negative = input < 0.0f;
+  if (negative)
+    input *= -1.0f;
 
-namespace MLink
-{
-    inline float ToFloat(int input)
-    {
-        const bool negative = 0 > input;
-        const float output  = negative ? (float) -input : (float) input;
-        return negative ? -output : output;
-    }
+  uint32_t output = static_cast<uint32_t>(input);
+  if (negative) {
+    output -= 0x80000000;
+    output = 0x80000000 - output;
+    output -= 1;
+  }
 
-    inline double ToDouble(int input)
-    {
-        const bool negative = input < 0;
-        const double output = (double) (negative ? -(uint32_t) input : (uint32_t) input);
-        return negative ? -output : output;
-    }
+  return output;
+}
 
-    inline uint32_t CorrectPointerOffset(uint32_t offset)
-    {
-#ifdef PLATFORM_CEMU
-        return offset;
-#else
-        return offset + 0x502200;
-#endif
-    }
+inline float ToFloat(int input) {
+  const bool negative = 0 > input;
+  const float output = negative ? (float)-input : (float)input;
+  return negative ? -output : output;
+}
 
-    template<typename T>
-    T *PointerFromAddress(uint32_t address)
-    {
-        return reinterpret_cast<T *>(address);
-    }
+inline double ToDouble(int input) {
+  const bool negative = input < 0;
+  const double output = (double)(negative ? -(uint32_t)input : (uint32_t)input);
+  return negative ? -output : output;
+}
 
-    template<typename T>
-    T *PointerFromOffset(uint32_t offset)
-    {
-        return PointerFromAddress<T>(CorrectPointerOffset(offset));
-    }
+inline uint32_t CorrectPointerOffset(uint32_t offset) { return offset; }
 
-    template<typename T>
-    T *DereferencePointerFromAddress(uint32_t address)
-    {
-        return *reinterpret_cast<T **>(address);
-    }
+template <typename T> T *PointerFromAddress(uint32_t address) {
+  return reinterpret_cast<T *>(address);
+}
 
-    template<typename T>
-    T *DereferencePointerFromOffset(uint32_t offset)
-    {
-        return DereferencePointerFromAddress<T>(CorrectPointerOffset(offset));
-    }
+template <typename T> T *PointerFromOffset(uint32_t offset) {
+  return PointerFromAddress<T>(CorrectPointerOffset(offset));
+}
+
+template <typename T> T *DereferencePointerFromAddress(uint32_t address) {
+  return *reinterpret_cast<T **>(address);
+}
+
+template <typename T> T *DereferencePointerFromOffset(uint32_t offset) {
+  return DereferencePointerFromAddress<T>(CorrectPointerOffset(offset));
+}
 } // namespace MLink
